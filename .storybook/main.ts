ee471110269e1d79517981c024ed87da556d2d6b
Aleksey Trubitsyn <aleksei.trubitsyn@gmail.com>
2026-03-10 23:08:01 +0400

    packages update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,13 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
   typescript: {
     check: false,
     checkOptions: {},
@@ -17,6 +16,6 @@ const config: StorybookConfig = {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
-  },
+  }
 }
 export default config
